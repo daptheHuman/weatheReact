af3254c1c5528e5d68ff2c3a29d6b5a61bc2754c
Dafa <dafaaqilla72@gmail.com>
2022-02-27 10:54:02 +0700

    feat: add popup modal

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -5,12 +5,13 @@ import { getLongLat } from '../services/location';
 
 const Searchbar = () => {
   const [location, setLocation] = useState('');
+  const [isOpened, setIsOpened] = useState(false);
   const navigate = useNavigate();
 
   const handleSearchButton = async (e) => {
     e.preventDefault();
     if (location === '') {
-      alert('Please enter an location');
+      setIsOpened(true);
     } else {
       const result = await getLongLat(location);
       navigate(
@@ -20,24 +21,46 @@ const Searchbar = () => {
   };
 
   return (
-    <div className="m-4 mx-auto flex w-1/2 flex-row gap-2 rounded-lg bg-baby-blue p-2 md:w-1/3">
-      <button type="button" onClick={(e) => handleSearchButton(e)}>
-        <SearchIcon className="h-5 w-5 text-white" />
-      </button>
+    <>
+      {/* Modal */}
+      <div
+        className={`fixed inset-0 z-20 flex h-full w-full flex-col items-center justify-center bg-gray-900/50 ${
+          isOpened ? '' : 'hidden'
+        }`}
+      >
+        <div className="flex h-1/4 w-1/2 flex-col items-center justify-between overflow-hidden rounded-lg bg-slate-100 md:w-1/3">
+          <span className="flex grow items-center text-xl font-bold text-slate-800 ">
+            Please enter a location
+          </span>
+          <button
+            className="w-full bg-star-blue py-3 text-sm text-white"
+            type="button"
+            onClick={() => setIsOpened(false)}
+          >
+            CLOSE
+          </button>
+        </div>
+      </div>
 
-      <input
-        className="grow bg-transparent text-white placeholder:text-white focus:outline-0"
-        type="text"
-        placeholder="Enter the location"
-        value={location}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            handleSearchButton(e);
-          }
-        }}
-        onChange={(e) => setLocation(e.target.value)}
-      />
-    </div>
+      <div className="m-4 mx-auto flex w-1/2 flex-row gap-2 rounded-lg bg-baby-blue p-2 md:w-1/3">
+        <button type="button" onClick={(e) => handleSearchButton(e)}>
+          <SearchIcon className="h-5 w-5 text-white" />
+        </button>
+
+        <input
+          className="grow bg-transparent text-white placeholder:text-white focus:outline-0"
+          type="text"
+          placeholder="Enter the location"
+          value={location}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleSearchButton(e);
+            }
+          }}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+      </div>
+    </>
   );
 };
 
